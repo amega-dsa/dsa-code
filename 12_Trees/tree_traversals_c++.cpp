ce92f6331c23e330a1276eed69a108b864d5af54
Satyamshukla007901 <bt20cse007@iiitn.ac.in>
2021-12-09 20:16:10 +0530

    added the main function , all the traversal functions are working fine

--- 12_Trees/tree_traversals_c++.cpp
+++ 12_Trees/tree_traversals_c++.cpp
@@ -66,3 +66,30 @@ void levelOrder(Node *root) // Recursive Level Order Traversal
             q.push(NULL);
     }
 }
+int32_t main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    //preorder traversal
+    preorder(root);
+    cout << endl;
+
+    //inorder traversal
+    inorder(root);
+    cout << endl;
+
+    //postorder traversal
+    postorder(root);
+    cout << endl;
+
+    //levelOrder traversal
+    levelOrder(root);
+    cout << endl;
+}
